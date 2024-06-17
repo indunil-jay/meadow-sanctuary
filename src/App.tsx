70b63371f65bb40f28321700d8f3acc162b4ad28
indunil-jay <indunil.jayz@gmail.com>
2024-06-17 21:30:22 +0530

    create app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import GlobalStyles from "./GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import AppLayout from "./layouts/AppLayout";
+
 const App = () => {
-  return <h1>initial</h1>;
+  return (
+    <>
+      <GlobalStyles />
+
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+          <Route path="*" element={<p>Page Not Found</p>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
